cc1422c87d86fd4edc18060e6e626b02e9804d45
ken-aqua <admin@admin-no-MacBook-Air.local>
2016-06-12 22:44:40 +0900

    add Cesium

---
+++ Cesium-1.22/Apps/Sandcastle/gallery/gallery-index.js
@@ -0,0 +1,242 @@
+// This file is automatically rebuilt by the Cesium build process.
+var gallery_demos = [{
+  "name": "3D Models",
+  "date": 1464709639805,
+  "img": "3D Models.jpg"
+}, {
+  "name": "ArcGIS MapServer",
+  "date": 1458920440263,
+  "img": "ArcGIS MapServer.jpg"
+}, {
+  "name": "Atmosphere Color",
+  "date": 1464789662351,
+  "img": "Atmosphere Color.jpg"
+}, {
+  "name": "Billboards",
+  "date": 1456937705745,
+  "img": "Billboards.jpg"
+}, {
+  "name": "Box",
+  "date": 1456937705752,
+  "img": "Box.jpg"
+}, {
+  "name": "Camera Tutorial",
+  "date": 1456937705913,
+  "img": "Camera Tutorial.jpg"
+}, {
+  "name": "Camera",
+  "date": 1456937705919,
+  "img": "Camera.jpg"
+}, {
+  "name": "Cardboard",
+  "date": 1456937705923,
+  "img": "Cardboard.jpg"
+}, {
+  "name": "Cesium Inspector",
+  "date": 1458920440275,
+  "img": "Cesium Inspector.jpg"
+}, {
+  "name": "Cesium Widget",
+  "date": 1456937705937,
+  "img": "Cesium Widget.jpg"
+}, {
+  "name": "Circles and Ellipses",
+  "date": 1456937705942,
+  "img": "Circles and Ellipses.jpg"
+}, {
+  "name": "Corridor",
+  "date": 1456937705948,
+  "img": "Corridor.jpg"
+}, {
+  "name": "Custom DataSource",
+  "date": 1456937705954,
+  "img": "Custom DataSource.jpg"
+}, {
+  "name": "Cylinders and Cones",
+  "date": 1456937705960,
+  "img": "Cylinders and Cones.jpg"
+}, {
+  "name": "CZML Billboard and Label",
+  "date": 1456937705759,
+  "img": "CZML Billboard and Label.jpg"
+}, {
+  "name": "CZML Circles and Ellipses",
+  "date": 1456937705768,
+  "img": "CZML Circles and Ellipses.jpg"
+}, {
+  "name": "CZML Colors",
+  "date": 1456937705779,
+  "img": "CZML Colors.jpg"
+}, {
+  "name": "CZML Model - Node Transformations",
+  "date": 1456937705788,
+  "img": "CZML Model - Node Transformations.jpg"
+}, {
+  "name": "CZML Model",
+  "date": 1456937705796,
+  "img": "CZML Model.jpg"
+}, {
+  "name": "CZML Path",
+  "date": 1458920440270,
+  "img": "CZML Path.jpg"
+}, {
+  "name": "CZML Point - Time Dynamic",
+  "date": 1456937705817,
+  "img": "CZML Point - Time Dynamic.jpg"
+}, {
+  "name": "CZML Point",
+  "date": 1456937705825,
+  "img": "CZML Point.jpg"
+}, {
+  "name": "CZML Polygon - Interpolating References",
+  "date": 1456937705834,
+  "img": "CZML Polygon - Interpolating References.jpg"
+}, {
+  "name": "CZML Polygon - Intervals, Availability",
+  "date": 1456937705842,
+  "img": "CZML Polygon - Intervals, Availability.jpg"
+}, {
+  "name": "CZML Polygon",
+  "date": 1462900376622,
+  "img": "CZML Polygon.jpg"
+}, {
+  "name": "CZML Polyline",
+  "date": 1462900376622,
+  "img": "CZML Polyline.jpg"
+}, {
+  "name": "CZML Position Definitions",
+  "date": 1456937705866,
+  "img": "CZML Position Definitions.jpg"
+}, {
+  "name": "CZML Rectangle",
+  "date": 1462481170832,
+  "img": "CZML Rectangle.jpg"
+}, {
+  "name": "CZML Reference Properties",
+  "date": 1456937705884,
+  "img": "CZML Reference Properties.jpg"
+}, {
+  "name": "CZML Spheres and Ellipsoids",
+  "date": 1456937705891,
+  "img": "CZML Spheres and Ellipsoids.jpg"
+}, {
+  "name": "CZML Wall",
+  "date": 1456937705899,
+  "img": "CZML Wall.jpg"
+}, {
+  "name": "CZML",
+  "date": 1456937705907,
+  "img": "CZML.jpg"
+}, {
+  "name": "GeoJSON and TopoJSON",
+  "date": 1456937705966,
+  "img": "GeoJSON and TopoJSON.jpg"
+}, {
+  "name": "GeoJSON simplestyle",
+  "date": 1456937705972,
+  "img": "GeoJSON simplestyle.jpg"
+}, {
+  "name": "Geometry and Appearances",
+  "date": 1456937705978,
+  "img": "Geometry and Appearances.jpg"
+}, {
+  "name": "Hello World",
+  "date": 1456937705983,
+  "img": "Hello World.jpg"
+}, {
+  "name": "Imagery Adjustment",
+  "date": 1456937705989,
+  "img": "Imagery Adjustment.jpg"
+}, {
+  "name": "Imagery Layers Manipulation",
+  "date": 1458920440279,
+  "img": "Imagery Layers Manipulation.jpg"
+}, {
+  "name": "Imagery Layers",
+  "date": 1458920440284,
+  "img": "Imagery Layers.jpg"
+}, {
+  "name": "Interpolation",
+  "date": 1458920440288,
+  "img": "Interpolation.jpg"
+}, {
+  "name": "KML",
+  "date": 1456937706013,
+  "img": "KML.jpg"
+}, {
+  "name": "Labels",
+  "date": 1456937706018,
+  "img": "Labels.jpg"
+}, {
+  "name": "Map Pins",
+  "date": 1456937706024,
+  "img": "Map Pins.jpg"
+}, {
+  "name": "Materials",
+  "date": 1456937706030,
+  "img": "Materials.jpg"
+}, {
+  "name": "PATerrain",
+  "date": 1458920440293,
+  "img": "PATerrain.jpg"
+}, {
+  "name": "Picking",
+  "date": 1456937706035,
+  "img": "Picking.jpg"
+}, {
+  "name": "Points",
+  "date": 1456937706041,
+  "img": "Points.jpg"
+}, {
+  "name": "Polygon",
+  "date": 1462900376622,
+  "img": "Polygon.jpg"
+}, {
+  "name": "Polyline Volume",
+  "date": 1456937706054,
+  "img": "Polyline Volume.jpg"
+}, {
+  "name": "Polyline",
+  "date": 1456937706059,
+  "img": "Polyline.jpg"
+}, {
+  "name": "Rectangle",
+  "date": 1456937706066,
+  "img": "Rectangle.jpg"
+}, {
+  "name": "Shadows",
+  "date": 1464709639810,
+  "img": "Shadows.jpg"
+}, {
+  "name": "Show or Hide Entities",
+  "date": 1456937706072,
+  "img": "Show or Hide Entities.jpg"
+}, {
+  "name": "Spheres and Ellipsoids",
+  "date": 1456937706077,
+  "img": "Spheres and Ellipsoids.jpg"
+}, {
+  "name": "Star Burst",
+  "date": 1456937706083,
+  "img": "Star Burst.jpg"
+}, {
+  "name": "Terrain Exaggeration",
+  "date": 1458920440299,
+  "img": "Terrain Exaggeration.jpg"
+}, {
+  "name": "Terrain",
+  "date": 1464709639817,
+  "img": "Terrain.jpg"
+}, {
+  "name": "Video",
+  "date": 1456937706105,
+  "img": "Video.jpg"
+}, {
+  "name": "Wall",
+  "date": 1456937706115,
+  "img": "Wall.jpg"
+}, {
+  "name": "Web Map Service (WMS)",
+  "date": 1458920440309,
+  "img": "Web Map Service (WMS).jpg"
+}];
